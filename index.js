2b02edc6c57361079993b3acdb083ff571df211d
Sheldon Fam <sheldonfam95@gmail.com>
2022-04-20 14:33:16 +0800

    updated js function

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ let mass = document.getElementById("mass");
 
 function pEnter() {
   num = type.value;
-  calculate();
+  convertMeters();
+  convertLiters();
+  convertKilo();
 }
 
-function calculate() {
-  let meters = (num * 0.3048).toFixed(3);
-  let feet = (num * 3.2808).toFixed(3);
-  length.textContent = `${num} meters = ${feet} feet | ${num} feet= ${meters} meters`;
-  let liters = (num * 0.264172).toFixed(3);
-  let gallons = (num * 3.78541).toFixed(3);
+function convertMeters() {
+  let meters = (num * 0.3048).toFixed(2);
+  let feet = (num * 3.2808).toFixed(2);
+  length.textContent = `${num} meters = ${feet} feet | ${num} feet = ${meters} meters`;
+}
+
+function convertLiters() {
+  let liters = (num * 0.264172).toFixed(2);
+  let gallons = (num * 3.78541).toFixed(2);
   volume.textContent = `${num} liters = ${liters} gallons | ${num} gallons = ${gallons} liters`;
-  let kilos = (num * 2.20462).toFixed(3);
-  let pounds = (num * 0.453592).toFixed(3);
+}
+
+function convertKilo() {
+  let kilos = (num * 2.20462).toFixed(2);
+  let pounds = (num * 0.453592).toFixed(2);
   mass.textContent = `${num} kilos = ${kilos} pounds | ${num} pounds = ${pounds} kilos`;
 }
-
-calculate();
